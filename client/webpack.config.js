29e477828fed921e60960aa5674db97bba633325
Reiley Burnitt <burnittreiley@gmail.com>
2023-10-11 11:44:50 -0600

    webpack configured

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,53 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Jate',
+      }),
+      new InjectManifest({
+        swSrc: './srcc-sw.js',
+        swDest: 'sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        name: 'Jate',
+        inject: true,
+        description: 'A simple text editor',
+        background_color: '#000000',
+        theme_color: '#ffffff',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+            {
+                src: path.resolve('src/images/logo.png'),
+                sizes: [96, 128, 256, 384, 512],
+                destination: path.join('assets', 'icons'),
+            },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+            test: /\.css$/i,
+            use: [
+                'style-loader',
+                'css-loader',
+            ],
+        },
+        {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-runtime']
+                },
+            },
+        },
       ],
     },
   };
